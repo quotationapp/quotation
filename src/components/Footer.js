bff1a9badb0e7ea8141c417dbe50cacac1a74fb4
Felipe Rego <feliperrego@gmail.com>
2017-11-21 17:05:31 -0300

    Add the basic structure to header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <ul>
+                    <li>
+                        <a href="about.html">about</a>
+                    </li>
+                    <li>
+                        <a href="privacy.html">privacy</a>
+                    </li>
+                    <li>
+                        <a href="terms.html">terms</a>
+                    </li>
+                </ul>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import Footer from "../components/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Quotation</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+          <Header title="quotation"/>
+          <Footer/>
+        </div>
     );
   }
 }
