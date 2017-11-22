bef7c40086f4c55ad574f5d63d0b4b85ae70b001
Felipe Rego <feliperrego@gmail.com>
2017-11-22 17:18:01 -0300

    Add link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+
     render () {
-        return  (
-            <header>
-                <h1>{this.props.title}</h1>
-            </header>
-        )
+
+        if (this.isHome()) {
+
+            return <header><h1>{this.props.title}</h1></header>;
+
+        } else {
+
+            return  (
+                <header>
+                    <h1><Link to="/">{this.props.title}</Link></h1>
+                </header>
+            )
+
+        }
+    }
+
+    isHome() {
+        return window.location.pathname === '/';
     }
 }
 
