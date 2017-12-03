84fddb24e0c8058f06c43ad299e1022fe8709bd2
Felipe Rêgo <feliperrego@gmail.com>
2017-12-03 13:57:14 -0300

    Added the conditional api URL

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -22,7 +22,7 @@ class Home extends Component {
 
     componentWillMount() {
 
-        axios.get('http://localhost:3001/currencies')
+        axios.get(window.apiURL + 'currencies')
             .then(response => {
                 this.setState({currencies: response.data});
                 this.setState({fromCurrency: this.findCurrency(this.state.defaultFromCurrency)});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
 
 import './assets/styles/main.css'
 
 import App from './containers/App';
+
+let apiURL;
+
+if (location.host === 'quotation.feliperego.com') {
+    apiURL = 'https://quotation-api.feliperego.com/';
+} else {
+    apiURL = 'http://localhost:3001/';
+}
+
+window.apiURL = apiURL;
 
 
 ReactDOM.render((
